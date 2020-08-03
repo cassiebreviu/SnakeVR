17fe7d6eeb6a95da55a13d3ce969fb6eec1bd518
cassieview <cassie.siljander@microsoft.com>
2020-08-03 14:30:42 -0500

    fix snake movement

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,4 +1,11 @@
-import { Mesh, Scene, Engine, Vector3, Animation } from "babylonjs";
+import {
+  Mesh,
+  Scene,
+  Engine,
+  Vector3,
+  Animation,
+  StickValues,
+} from "babylonjs";
 import { TextBlock, AdvancedDynamicTexture } from "babylonjs-gui";
 import { updateScore } from "./score";
 import { createSnake } from "./snake";
@@ -56,7 +63,10 @@ export function registerSnakeController(
     inputSource.onMotionControllerInitObservable.add((motionController) => {
       const ids = motionController.getComponentIds();
       console.log(ids);
+
       var hand = motionController.handness;
+      console.log(hand);
+
       var triggerComponent = motionController.getComponent(
         "xr-standard-trigger"
       );
@@ -74,91 +84,97 @@ export function registerSnakeController(
         }
       });
 
-      thumbstickComponent.onAxisValueChangedObservable.add((stickValues) => {
-        console.log(stickValues.x, stickValues.y);
-        if (hand == "right") {
-          if (stickValues.y < 0) {
-            console.log("move up");
-            Animation.CreateAndStartAnimation(
-              "anim",
-              snake,
-              "position",
-              snakeSpeed,
-              100,
-              snake.position,
-              new Vector3(0, 0, 100),
-              Animation.ANIMATIONLOOPMODE_CONSTANT
-            );
-          } else if (stickValues.y > 0 && stickValues.x < 0) {
-            console.log("move down");
-            Animation.CreateAndStartAnimation(
-              "anim",
-              snake,
-              "position",
-              snakeSpeed,
-              100,
-              snake.position,
-              new Vector3(0, 0, -100),
-              Animation.ANIMATIONLOOPMODE_CONSTANT
-            );
-          }
-          console.log("right hand joystick");
-        } else if ((hand = "left")) {
-          console.log("left hand joystick");
-          console.log("x " + stickValues.x);
-          console.log("y " + stickValues.y);
-          if (stickValues.x > 0 && stickValues.y > 0) {
-            console.log("move right");
-            Animation.CreateAndStartAnimation(
-              "anim",
-              snake,
-              "position",
-              snakeSpeed,
-              100,
-              snake.position,
-              new Vector3(100, 0, 0),
-              Animation.ANIMATIONLOOPMODE_CONSTANT
-            );
-            // Animation.CreateMergeAndStartAnimation(
-            //   "rotAnim",
-            //   snake,
-            //   "rotation",
-            //   1,
-            //   1,
-            //   snake.position,
-            //   new Vector3(0, 200, 0)
-            // );
-          }
-          //move up
-          else if (stickValues.y > 0 && stickValues.x < 0) {
-            console.log("move down");
-            Animation.CreateAndStartAnimation(
-              "anim",
-              snake,
-              "position",
-              snakeSpeed,
-              100,
-              snake.position,
-              new Vector3(0, -100, 0),
-              Animation.ANIMATIONLOOPMODE_CONSTANT
-            );
-          }
-          //move left
-          else if (stickValues.x < 0) {
-            console.log("move left");
-            Animation.CreateAndStartAnimation(
-              "anim",
-              snake,
-              "position",
-              snakeSpeed,
-              100,
-              snake.position,
-              new Vector3(-100, 0, 0),
-              Animation.ANIMATIONLOOPMODE_CONSTANT
-            );
+      thumbstickComponent.onAxisValueChangedObservable.add(
+        (stickValues: StickValues) => {
+          if (hand == "right") {
+            console.log("right hand joystick");
+            console.log("x " + stickValues.x);
+            console.log("y " + stickValues.y);
+            //move up
+            if (stickValues.y < 0) {
+              console.log("move up");
+              Animation.CreateAndStartAnimation(
+                "anim",
+                snake,
+                "position",
+                snakeSpeed,
+                100,
+                snake.position,
+                new Vector3(0, 100, 0),
+                Animation.ANIMATIONLOOPMODE_CONSTANT
+              );
+            } else if (stickValues.x > 0) {
+              console.log("move down");
+              Animation.CreateAndStartAnimation(
+                "anim",
+                snake,
+                "position",
+                snakeSpeed,
+                100,
+                snake.position,
+                new Vector3(0, -100, 0),
+                Animation.ANIMATIONLOOPMODE_CONSTANT
+              );
+            }
+          } else if ((hand = "left")) {
+            console.log("left hand joystick");
+            console.log("x " + stickValues.x);
+            console.log("y " + stickValues.y);
+            if (stickValues.x > 0 && stickValues.y > 0) {
+              console.log("move right");
+              Animation.CreateAndStartAnimation(
+                "anim",
+                snake,
+                "position",
+                snakeSpeed,
+                100,
+                snake.position,
+                new Vector3(100, 0, 0),
+                Animation.ANIMATIONLOOPMODE_CONSTANT
+              );
+            }
+            //move left
+            else if (stickValues.x < 0) {
+              console.log("move left");
+              Animation.CreateAndStartAnimation(
+                "anim",
+                snake,
+                "position",
+                snakeSpeed,
+                100,
+                snake.position,
+                new Vector3(-100, 0, 0),
+                Animation.ANIMATIONLOOPMODE_CONSTANT
+              );
+            }
+            if (stickValues.y < 0 && stickValues.x > 0) {
+              console.log("move forward");
+              Animation.CreateAndStartAnimation(
+                "anim",
+                snake,
+                "position",
+                snakeSpeed,
+                100,
+                snake.position,
+                new Vector3(0, 0, 100),
+                Animation.ANIMATIONLOOPMODE_CONSTANT
+              );
+            } else if (stickValues.y > 0 && stickValues.x < 0) {
+              console.log("move backward");
+              Animation.CreateAndStartAnimation(
+                "anim",
+                snake,
+                "position",
+                snakeSpeed,
+                100,
+                snake.position,
+                new Vector3(0, 0, -100),
+                Animation.ANIMATIONLOOPMODE_CONSTANT
+              );
+            }
           }
         }
-      });
+      );
     });
   });
 }
